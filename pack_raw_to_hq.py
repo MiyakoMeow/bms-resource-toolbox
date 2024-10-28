7303694540d13329994a5d2d88a4c1e54d7005a2
MiyakoMeow <miyakomeow@qq.com>
2024-10-28 10:55:20 +0800

    Add raw to HQ script

---
+++ pack_raw_to_hq.py
@@ -0,0 +1,29 @@
+from bms_media.audio import (
+    AUDIO_PRESET_FLAC,
+    AUDIO_PRESET_FLAC_FFMPEG,
+)
+import f_transfer_audio
+import f_remove_unneed_media_file
+
+
+def main():
+    print("This file is for parsing HQ version to LQ version. Just for LR2 players.")
+    root_dir = input("Input BMS Dir:")
+    # Parse Audio
+    print("Parsing Audio... Phase 1: WAV -> FLAC")
+    f_transfer_audio.main(
+        root_dir=root_dir,
+        input_ext=["wav"],
+        transfer_mode=[AUDIO_PRESET_FLAC, AUDIO_PRESET_FLAC_FFMPEG],
+        remove_origin_file=True,
+        skip_on_fail=True,
+    )
+    # Remove Unneed Media File
+    print("Removing Unneed Files")
+    f_remove_unneed_media_file.main(
+        root_dir, preset=f_remove_unneed_media_file.PRESET_NORMAL
+    )
+
+
+if __name__ == "__main__":
+    main()
