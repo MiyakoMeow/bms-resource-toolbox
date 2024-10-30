07fcf7568ff64a62e322e14c9f4819ce81c8fd53
MiyakoMeow <miyakomeow@qq.com>
2024-10-30 09:02:47 +0800

    Improved remove_empty_folder

--- f_remove_empty_folder.py
+++ f_remove_empty_folder.py
@@ -1,10 +1,22 @@
 import os
+import shutil
 
 from bms_fs import get_bms_folder_dir
 
 
-def is_dir_empty(dir_path: str) -> bool:
-    return len(os.listdir(dir_path)) == 0
+def is_dir_having_file(dir_path: str) -> bool:
+    has_file = False
+    for element_name in os.listdir(dir_path):
+        element_path = os.path.join(dir_path, element_name)
+        if os.path.isfile(element_path):
+            has_file = True
+        elif os.path.isdir(element_path):
+            has_file = has_file or is_dir_having_file(element_path)
+
+        if has_file:
+            break
+
+    return has_file
 
 
 def main(bms_dir: str = ""):
@@ -15,9 +27,9 @@ def main(bms_dir: str = ""):
         dir_path = os.path.join(bms_dir, dir_name)
         if not os.path.isdir(dir_path):
             continue
-        if is_dir_empty(dir_path):
+        if not is_dir_having_file(dir_path):
             print(f"Remove empty dir: {dir_path}")
-            os.rmdir(dir_path)
+            shutil.rmtree(dir_path)
 
 
 if __name__ == "__main__":
