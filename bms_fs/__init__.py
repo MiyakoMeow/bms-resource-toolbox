0b92036f9ab0ee27bd08d6f6859676fe0c2c2010
MiyakoMeow <miyakomeow@qq.com>
2024-11-09 14:27:09 +0800

    fix: Moving problems

--- bms_fs/__init__.py
+++ bms_fs/__init__.py
@@ -84,12 +84,23 @@ def move_elements_across_dir(
             print(f" - Moving from {ori_path} to {dst_path}")
         # Move
         if os.path.isfile(ori_path):
+            # Replace?
             if os.path.isfile(dst_path) and replace:
                 os.remove(dst_path)
+            # Move
             if not os.path.isfile(dst_path):
                 shutil.move(ori_path, dst_path)
         elif os.path.isdir(ori_path):
+            # Make new dir in dst
+            if not os.path.isdir(dst_path):
+                os.mkdir(dst_path)
+            # Child dir
             move_elements_across_dir(ori_path, dst_path, print_info, replace)
+
+    # Clean Source
+    if replace or not is_dir_having_file(dir_path_ori):
+        for e in os.listdir(dir_path_ori):
+            shutil.rmtree(os.path.join(dir_path_ori, e))
 
 
 def is_dir_having_file(dir_path: str) -> bool:
