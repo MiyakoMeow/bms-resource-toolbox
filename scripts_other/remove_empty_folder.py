1cf990c8da0b4f765a9c34943111cf428e6b38d5
MiyakoMeow <miyakomeow@qq.com>
2024-11-23 00:37:01 +0800

    mv: remove_empty_folder to scripts_other

--- scripts_other/remove_empty_folder.py
+++ scripts_other/remove_empty_folder.py
@@ -14,12 +14,12 @@ sys.path.append(f"{current_directory}/..")
 from bms_fs import get_bms_folder_dir, is_dir_having_file
 
 
-def main(bms_dir: str = ""):
-    if len(bms_dir) == 0:
-        bms_dir = get_bms_folder_dir()
+def main(parent_dir: str = ""):
+    if len(parent_dir) == 0:
+        parent_dir = get_bms_folder_dir()
 
-    for dir_name in os.listdir(bms_dir):
-        dir_path = os.path.join(bms_dir, dir_name)
+    for dir_name in os.listdir(parent_dir):
+        dir_path = os.path.join(parent_dir, dir_name)
         if not os.path.isdir(dir_path):
             continue
         if not is_dir_having_file(dir_path):
