00d407a74e0aeeb10ff70f820fa22541870b9219
MiyakoMeow <8192569+MiyakoMeow@user.noreply.gitee.com>
2025-04-17 10:42:45 +0800

    rename: set_name => copy_name

--- bms_folder_copy_name_with_num.py
+++ bms_folder_copy_name_with_num.py
@@ -3,6 +3,12 @@ import shutil
 
 
 def main(src_dir: str = "", dst_dir: str = ""):
+    """
+    该脚本使用于以下情况：
+    已经有一个文件夹A，它的子文件夹名为“”等带有编号+小数点的形式。
+    现在有另一个文件夹B，它的子文件夹名都只有编号。
+    将A中的子文件夹名，同步给B的对应的子文件夹。
+    """
     if len(src_dir) == 0:
         src_dir = input("Input Src Dir:")
     if len(dst_dir) == 0:
