b54b3c7eb8711a2011d31f131dd99cca58f73caa
MiyakoMeow <8192569+MiyakoMeow@user.noreply.gitee.com>
2025-04-21 02:34:14 +0800

    add: bigpack_folder_move

---
+++ bigpack_folder_move.py
@@ -0,0 +1,29 @@
+import os
+
+from bms_fs import MoveOptions, move_elements_across_dir
+
+
+def main(src_dir: str, dst_dir: str):
+    for bms_dir_name in os.listdir(src_dir):
+        bms_dir = os.path.join(src_dir, bms_dir_name)
+        if not os.path.isdir(bms_dir):
+            continue
+
+        print(f"Moving: {bms_dir_name}")
+
+        dst_bms_dir = os.path.join(dst_dir, bms_dir_name)
+        move_elements_across_dir(
+            bms_dir,
+            dst_bms_dir,
+            MoveOptions(
+                replace=True,
+                replace_skip_unique_file=True,
+                replace_save_both_unique_file=True,
+            ),
+        )
+
+
+if __name__ == "__main__":
+    src_dir = input("Input the src dir:")
+    dst_dir = input("Input the dst dir:")
+    main(src_dir, dst_dir)
